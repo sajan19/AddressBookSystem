0877c2f941af8450396bb754d479eab33a9d2e2e
Sajan <sajanmhatre29@gmail.com>
2021-09-06 01:21:57 +0530

    Add Multiple Person Contact

--- Address Book System/src/AddressBookMain.java
+++ Address Book System/src/AddressBookMain.java
@@ -1,5 +1,11 @@
+import java.util.Scanner;
+
 public class AddressBookMain {
+    // main method
     public static void main(String[] args) {
-        System.out.println("Welcome to Address Book Program");
+        System.out.println("Welcome to Address Book System ! ");
+        Scanner sc = new Scanner(System.in);
+        AddressBook addressBook = new AddressBook();
+        addressBook.getMenu();
     }
 }
